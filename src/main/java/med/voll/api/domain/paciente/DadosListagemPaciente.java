7e0a0e27e6b8e7a568ec66827e74890fd376e8c7
Henrique Diniz <henriquee.diniz@hotmail.com>
2023-05-08 19:58:53 -0300

    fixing security issues

--- src/main/java/med/voll/api/domain/paciente/DadosListagemPaciente.java
+++ src/main/java/med/voll/api/domain/paciente/DadosListagemPaciente.java
@@ -1,9 +1,7 @@
-package med.voll.api.paciente;
-
-import jakarta.validation.Valid;
-import med.voll.api.endereco.DadosEndereco;
+package med.voll.api.domain.paciente;
 
 public record DadosListagemPaciente(Long id, String nome, String email, String cpf) {
+
     public DadosListagemPaciente(Paciente paciente) {
         this(paciente.getId(), paciente.getNome(), paciente.getEmail(), paciente.getCpf());
     }
